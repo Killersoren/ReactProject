01f577d93d85a86865062303639527f1bfd687c3
mrkoldbrandt <mrkoldbrandt@gmail.com>
2022-02-14 12:26:10 +0100

    NPCCreator example

---
+++ chargen/src/App.js
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+
+function NPCCreatorWithHooks() {
+  let name = useFormInput("");
+  let faction = useFormInput("");
+  let reputation = useFormInput("Friendly");
+
+  return (
+    <>
+      <h3>NPC Creator</h3>
+      <NPCForm name={name} faction={faction} reputation={reputation} />
+      <h5>Result:</h5>
+      <NameUI name={name.value} faction={faction.value} reputation={reputation.value} />
+    </>
+  );
+}
+
+function useFormInput(initialValue) {
+  const [value, setValue] = useState(initialValue);
+  
+  function handleChange(event) {
+    setValue(event.target.value);
+  }
+
+  return { value: value, onChange: handleChange };
+}
+
+function NPCForm(props) {
+  return (
+    <>
+      <label>Name:</label>
+      <br />
+      <input {...props.name}></input>
+      <br />
+      <label>Faction:</label>
+      <br />
+      <input {...props.faction}></input>
+      <br />
+      <label>Reputation:</label>
+      <br />
+      <select {...props.reputation}>
+        <option>Friendly</option>
+        <option>Neutral</option>
+        <option>Hostile</option>
+      </select>
+    </>
+  );
+}
+
+function NameUI(props) {
+  function getReputationColor(reputation) {
+    if (reputation === "Friendly") return "green";
+    if (reputation === "Neutral") return "orange";
+    if (reputation === "Hostile") return "Red";
+  }
+
+  return (
+    <div
+      style={{
+        width: "250px",
+        color: getReputationColor(props.reputation),
+        fontWeight: "bolder",
+        textAlign: "center"
+      }}>
+      <div>{props.name}</div>
+      <div>{"<" + props.faction + ">"}</div>
+    </div>
+  );
+}
+
+export default NPCCreatorWithHooks;
