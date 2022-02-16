0536c2b91c349f418e71594c61fff287fdad82be
mrkoldbrandt <mrkoldbrandt@gmail.com>
2022-02-16 18:28:31 +0100

    stats + ild

--- chargen/src/NameInput.js
+++ chargen/src/NameInput.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-const names=['Bo', 'Hans', 'Lars']
+import { playerNames } from "./names";
+//const names=['Bo', 'Hans', 'Lars']
 
 function NameInput(props) {
   const [value, setValue] = useState("");
@@ -11,17 +12,21 @@ function NameInput(props) {
   return (
     <div id="RandomName-container"style={{
       backgroundColor: 'grey',
-      width: '100px',
-      height: '100px'
+      width: '200px',
+      height: '100px',
+      position: "absolute",
+      left: 950 ,
+      top: 50,
+     backgroundImage: `url(${"https://png.pngtree.com/thumb_back/fw800/background/20190827/pngtree-fire-texture-background-with-red-hot-burning-flames-image_306674.jpg"})`
+      
     }}>
     
       <input value={value} onChange={handleChange} />
-      <button onClick={() => setValue(names[Math.floor(Math.random()*names.length)])}>
+      <button onClick={() => setValue(playerNames[Math.floor(Math.random()*playerNames.length)] +" "+ playerNames[Math.floor(Math.random()*playerNames.length)])}>
             New Random Name
         </button>
       <p>Name: {value}</p>
-     
-
+      
     </div>
   );
 }
