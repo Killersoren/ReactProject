3364b3b25c21d2eacdaa7ff5f9be27688495cd8e
Søren Jørgensen <skmj@hotmail.dk>
2022-02-17 02:30:27 +0100

    Random classes API stuff virker :sunglasses:

--- chargen/src/ClassSelection.js
+++ chargen/src/ClassSelection.js
@@ -1,14 +1,13 @@
 import React, {useState} from "react";
 
- const classes=['archer', 'warrier', 'warlock'];
-let classElement = document.getElementById("class");
-
 const url = "https://www.dnd5eapi.co/api/classes";
+let classesList = [];
+getRandomClass();
 
-let classesList =[];
-// let characterClass = "";
 
-function RandomClass() { const [characterClass, newClass] = useState('archer');
+
+
+function RandomClass() { const [ characterClass, newClass] = useState('archer');
 
     return (
         <div id="RandomClass-container"style={{
@@ -16,22 +15,12 @@ function RandomClass() { const [characterClass, newClass] = useState('archer');
             width: '100px',
             height: '100px'
           }}>
-            
+    
             <h2>{characterClass}</h2>
 
-        <button onClick={() => {
-            getRandomClass();
-            console.log(classesList);
-            newClass(classesList[Math.floor(Math.random()*classesList.length)])
-            }
-
-          
-            
+        <button onClick={() => newClass(classesList[Math.floor(Math.random()*classesList.length)].name)
+  
         }  >
-
-
-        {/* <button onClick={() =>   newClass(classes[Math.floor(Math.random()*classes.length)])}> */}
-        {/* <button onClick={() => getRandomClass()}> */}
 
             New Class
         </button>
@@ -40,32 +29,19 @@ function RandomClass() { const [characterClass, newClass] = useState('archer');
     );
 }
 
+
+
  async function getRandomClass()
 {
     const response = await fetch(url);
     const data = await response.json();
 
     classesList = data.results;
-    // console.log("classes list =" + classesList);
-    // console.log("character class = " + characterClass);
 
-    // (data.results[Math.floor(Math.random()*data.results.length)])
 
-    return data;
-
-    // let tmpResult;
-
-    // tmpResult = data.results[0];
-
-    //    this.setState({characterClass: data.results[3]})
-
-    // console.log(tmpResult);
-    // console.log(data.results[0]);
-
-    // return fetch(url)
-    // .then(response => response.json())
-    // .then(classObject => classObject.)
+    return classesList;
 }
+
 
 
 export default RandomClass;
